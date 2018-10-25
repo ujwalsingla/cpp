597d7e26ed7c4da65d0d13522feeaa8e0f18337e
ujwalsingla <44477401+ujwalsingla@users.noreply.github.com>
2018-10-25 23:23:38 +0530

    Update

--- avg.cpp
+++ avg.cpp
@@ -7,7 +7,7 @@ int main() {
     float average;
     cout << "Enter 3 numbers: " << endl;
     cin >> a >> b >> c;
-    average = float(a + b + c)/3.0;
+    average = float(a + b + c)/3.0;//average is calculated
     cout << "Average = " << average << endl;
 
     return 0;
